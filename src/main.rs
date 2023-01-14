a2a743fe6d4a434b7e91e41a9f57b453388a5cc8
Tillwege <tillwege@gmail.com>
2023-01-14 23:46:38 +0100

    added progress indicator

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,24 @@ const IMAGE_WIDTH: u32 = 256;
 const IMAGE_HEIGHT: u32 = 256;
 
 fn main() {
-    let image = image::ImageBuffer::from_fn(IMAGE_WIDTH, IMAGE_HEIGHT, |x, y| {
-        let r = x as f32 / (IMAGE_WIDTH - 1) as f32;
-        let g = y as f32 / (IMAGE_HEIGHT - 1) as f32;
-        let b = 0.25;
+    let mut image = image::ImageBuffer::new(IMAGE_WIDTH, IMAGE_HEIGHT);
 
-        image::Rgb([(r * 255.99) as u8, (g * 255.99) as u8, (b * 255.99) as u8])
-    });
+    
+    for y in 0..IMAGE_HEIGHT {
+        println!("Rendering line {}", y);
+        for x in 0..IMAGE_WIDTH {
+            let pixel = image.get_pixel_mut(x, y);
+            
+            let r = x as f32 / (IMAGE_WIDTH - 1) as f32;
+            let g = y as f32 / (IMAGE_HEIGHT - 1) as f32;
+            let b = 0.25;
+
+            *pixel = image::Rgb([(r * 255.99) as u8, (g * 255.99) as u8, (b * 255.99) as u8]);
+        }
+    }
+
+    
+
 
     if let Err(e) = image.save("image.png") {
         println!("Error saving image: {}", e);
